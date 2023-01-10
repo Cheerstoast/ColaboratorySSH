ac6bc57ccd5d15b7e6858670e45e72664c6aae3f
Cheerstoast <cheerstoast@gmail.com>
2023-01-10 21:49:09 +1100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,8 +2,7 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Download ngrok"
 echo "======================="
-wget -O ngrok.zip  
-https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken K7sHWI1hCsgCWtGrzyFoV7UYJr_4Uuvnt1k33VWHnEzJ8u7m
 clear
