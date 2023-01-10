eac3a0005b8a6c3db854972e0a7eb95ffd9de2e3
Cheerstoast <cheerstoast@gmail.com>
2023-01-10 21:36:25 +1100

    Add files via upload

---
+++ ngrok.sh
@@ -0,0 +1,28 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip  
+https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken K7sHWI1hCsgCWtGrzyFoV7UYJr_4Uuvnt1k33VWHnEzJ8u7m
+
+clear
+
+./ngrok tcp --region au 22 &>/dev/null &
+echo "======================="
+echo Updating Please Wait
+echo "======================="
+sudo apt update > /dev/null 2>&1
+sudo apt install openssh-server > /dev/null 2>&1
+mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+sudo service ssh start
+echo "===================================="
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo create root password
+passwd
+echo "===================================="
